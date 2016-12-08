8eda3be209481db8b1338417ceb89925e5b745dd
spinclick <spinclick@users.noreply.github.com>
2016-12-08 18:14:40 -0500

    better read functions

--- extensions.ml
+++ extensions.ml
@@ -13,12 +13,23 @@ module List = struct
       else _make (succ i) (fn () :: lst)
     in
     List.rev @@ _make 0 [];;
+
+  let sum lst = 
+    List.fold_left (fun a b -> a + b) 0 lst
+
 end;;
+
+module Array = struct
+  include Array
+
+  let sum lst = 
+    Array.fold_left (fun a b -> a + b) 0 lst ;;
+
+end;;
+
 
 module Pervasives = struct
   include Pervasives
-
-  let read_byte = input_byte ;;
 
   let input_byte_array nbytes in_ch = 
     let arr = Array.make nbytes 0 in
@@ -36,31 +47,26 @@ module Pervasives = struct
 
   (* TODO: Remove duplicate code in read_int *)
 
-  let read_int16 in_ch = 
-    Array.fold_right
-      (fun init elem -> init + elem)
-      (Array.mapi 
-        (fun offset elem -> elem lsl (offset*8)) 
-        (input_byte_array 2 in_ch)) 
-      0 ;;
+  let read_i16 in_ch = 
+    let b1 = input_byte in_ch in
+    let b2 = input_byte in_ch in
+    b1 lor (b2 lsl 8) ;;
 
-  let read_int32 in_ch = 
+  let read_i32 in_ch = 
     let open Int32 in
-    Array.fold_right
-      (fun init elem -> add init elem)
-      (Array.mapi 
-        (fun offset elem -> shift_left (of_int elem) (offset*8)) 
-        (input_byte_array 4 in_ch)) 
-      zero ;;
+    let ( ||| ) = logor in
+    let ( <<< ) = shift_left in
+    let i16_1 = of_int @@ read_i16 in_ch in
+    let i16_2 = of_int @@ read_i16 in_ch in
+    i16_1 ||| (i16_2 <<< 16) ;;
 
-  let read_int64 in_ch = 
+  let read_i64 in_ch =
     let open Int64 in
-    Array.fold_right
-      (fun init elem -> add init elem)
-      (Array.mapi 
-        (fun offset elem -> shift_left (of_int elem) (offset*8)) 
-        (input_byte_array 8 in_ch)) 
-      zero ;;
+    let ( ||| ) = logor in
+    let ( <<< ) = shift_left in
+    let i32_1 = of_int32 @@ read_i32 in_ch in
+    let i32_2 = of_int32 @@ read_i32 in_ch in
+    i32_1 ||| (i32_2 <<< 32) ;;
 
   let read_single in_ch = 
     Int32.to_float @@ read_int32 in_ch ;;
@@ -69,32 +75,22 @@ module Pervasives = struct
     Int64.to_float @@ read_int64 in_ch ;;
 
   let read_int32_array in_ch n = 
-    let arr = Array.make n Int32.zero in
-    for i=0 to (n-1) do
-      arr.(i) <- (read_int32 in_ch)
-    done;
-    arr ;;
+    Array.init (fun i -> read_int32 in_ch) n ;;
 
   let read_bool_array in_ch n = 
-    let arr = Array.make n false in
-    for i=0 to (n-1) do
-      arr.(i) <- (read_bool in_ch)
-    done;
-    arr ;;
+    Array.init (fun i -> read_bool in_ch) n ;;
 
   let read_string_array in_ch n = 
-    let arr = Array.make n "" in
-    for i=0 to (n-1) do
-      arr.(i) <- (read_pascal_string in_ch)
-    done;
-    arr ;;
+    Array.init (fun i -> read_pascal_string in_ch) n ;;
 
   let read_byte_array in_ch n = 
-    let arr = Array.make n 0 in
-    for i=0 to (n-1) do
-      arr.(i) <- (input_byte in_ch)
-    done;
-    arr ;;
+    Array.init (fun i -> input_byte in_ch) n ;;
+
+  (* Integer exponent *)
+  let rec ( ^^ ) base exp =
+    if exp = 0 then 1
+    else if exp = 1 then base
+    else (base*(base ^^ (exp-1))) ;;
 
 end;;
 
